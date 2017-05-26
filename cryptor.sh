edae7f0ab385038c3ca65d9d71aaebce1c971496
Adam Borowski <adam@adamboro.com>
2017-05-26 21:13:53 +0200

    ask for password confirmation on encrypting

--- cryptor.sh
+++ cryptor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# #!/bin/bash
 
 # usage:
 # bash cryptor.sh <PASSWORD>
@@ -7,31 +7,42 @@ files_folder="files"
 tar_filename="files.tar"
 files_encoded="files.enc"
 
-# if no password is provided, ask for one
-if [ ! "$1" ]; then
-  echo 'pass:'
-  read -s password
-else
-  password="$1"
-fi
-
 # check if folder or encrypted file exists
 if [ -d "$files_folder" ]; then
   echo "encryption mode";
-  mode="enc"
+  MODE="enc"
 elif [ -f "$files_encoded" ]; then
   echo "decryption mode";
-  mode="dec"
+  MODE="dec"
 else
   echo "no files folder or encrypted file, exiting"; exit 1
 fi
 
-if [[ $mode == "enc" ]]; then
+# if no password is provided, ask for one
+if [ ! "$1" ]; then
+  # if encrypting, ask for confirmation
+  if [ "$MODE" = "enc" ]; then
+    while true; do
+      read -s -p "Password: " PASSWORD
+      echo
+      read -s -p "Password (again): " PASSWORD2
+      echo
+      [ "$PASSWORD" = "$PASSWORD2" ] && break
+      echo "Please try again"
+    done
+  else
+    read -s -p "Password: " PASSWORD
+  fi
+else
+  PASSWORD="$1"
+fi
+
+if [[ $MODE == "enc" ]]; then
   # archive the folder
   tar -cf $tar_filename $files_folder
 
   # encrypt
-  if openssl enc -aes256 -a -in $tar_filename -out $files_encoded -k $password ; then
+  if openssl enc -aes256 -a -in $tar_filename -out $files_encoded -k $PASSWORD ; then
     echo "encryption succeeded"
     rm -rf $files_folder $tar_filename
   else
@@ -39,7 +50,7 @@ if [[ $mode == "enc" ]]; then
   fi
 else
   # decrypt
-  if openssl enc -aes256 -a -d -in $files_encoded -out $tar_filename -k $password ; then
+  if openssl enc -aes256 -a -d -in $files_encoded -out $tar_filename -k $PASSWORD ; then
     echo "decryption succeeded"
     tar -xvf $tar_filename
     rm -rf $files_encoded $tar_filename
